e360d7b83ae4049a525e180bc43d3c417f3796d6
Maurice Gift <founder@giftedtech.co.ke>
2026-04-26 20:46:43 +0300

    Refactor and reorganize API index file

--- api/index.js
+++ api/index.js
@@ -73,6 +73,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.set('trust proxy', 1);
 app.set('json spaces', 2);
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, '../public')));
